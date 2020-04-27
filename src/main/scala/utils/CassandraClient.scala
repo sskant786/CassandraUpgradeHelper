a4304ce71d41fd7886a7640ad666f6786d2ff9a7
sskant786 <sardar.suryakant@gslab.com>
2020-04-27 14:44:21 +0530

    Made cassandra connection protocol configurable

--- src/main/scala/utils/CassandraClient.scala
+++ src/main/scala/utils/CassandraClient.scala
@@ -1,6 +1,6 @@
 package utils
 
-import com.datastax.driver.core.Cluster
+import com.datastax.driver.core.{Cluster, ProtocolVersion}
 import com.datastax.driver.core.policies.ConstantReconnectionPolicy
 
 /**
@@ -10,8 +10,21 @@ import com.datastax.driver.core.policies.ConstantReconnectionPolicy
 object CassandraClient {
 
   private val nodes = ConfigReader.getStringList("CASSANDRA_ENDPOINT").map(_.toList).getOrElse(List("127.0.0.1"))
+  private val protocolVersion = ConfigReader.getString("CASSANDRA_CONNECTION_PROTOCOL_VERSION").getOrElse("V3")
+  private val cassandraConnectionProtocol = protocolVersion.trim.toUpperCase match {
+    case "V1" => ProtocolVersion.V1
+    case "V2" => ProtocolVersion.V2
+    case "V3" => ProtocolVersion.V3
+    case "V4" => ProtocolVersion.V4
+    case "V5" => ProtocolVersion.V5
+    case _ => LogUtils.getLogger.warn(s"Protocol version [$protocolVersion] mentioned in conf is not a valid version. So using default version i.e V3")
+      ProtocolVersion.V3
+  }
 
-  private val cluster = Cluster.builder.addContactPoints(nodes: _*).withReconnectionPolicy(new ConstantReconnectionPolicy(1000L)).build
+  private val cluster = Cluster.builder
+    .addContactPoints(nodes: _*).withReconnectionPolicy(new ConstantReconnectionPolicy(1000L))
+    .withProtocolVersion(cassandraConnectionProtocol)
+    .build
 
   val session = cluster.connect()
 }
